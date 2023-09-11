9df93570e25d9553bafe87b003d0979ede9434ad
Febin Johnson <febinnj1996@gmail.com>
2023-09-12 00:04:11 +0530

    made into a clean format..

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
-import 'package:memo/features/home/home_screen.dart';
-import 'package:memo/features/splash/splash_screen.dart';
+import 'package:memo/presentation/home/home_screen.dart';
+import 'package:memo/presentation/splash/splash_screen.dart';
 
 List<GetPage> routes() =>
     [GetPage(name: '/', page: () => const SplashScreen()),
