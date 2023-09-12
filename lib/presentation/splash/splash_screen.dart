077660b71373721a4d0dbe584d5b827483455d3f
Febin Johnson <febinnj1996@gmail.com>
2023-09-12 23:39:26 +0530

    Added get_it package and added controller.

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:get/get.dart';
 import 'package:memo/config/app_color.dart';
+import 'package:memo/presentation/splash/splash_controller.dart';
 import 'package:memo/utils/constants.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -8,20 +10,24 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: Container(
           height: 200,
           width: 200,
           decoration: BoxDecoration(
-            color: frenchViolet,
-            borderRadius: BorderRadius.circular(100)
-          ),
-          child: Center(
-            child: Text("$appName.",
-            style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold,color: Colors.white),
-            ).animate().fadeIn().scale(delay: 1200.ms),
-          ),
+              color: frenchViolet, borderRadius: BorderRadius.circular(100)),
+          child: GetBuilder<SplashController>(
+              init: SplashController(),
+              builder: (controller) {
+                return Center(
+                  child: Text(
+                    "$appName.",
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ).animate().fadeIn().scale(delay: 1200.ms),
+                );
+              }),
         ),
       ),
     );
