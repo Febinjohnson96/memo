9c482b226e464023c775c4cca1aee11c6cf41495
Febin Johnson <febinnj1996@gmail.com>
2023-09-12 21:01:45 +0530

    added splash screen + fixes (#3)

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:memo/config/app_color.dart';
+import 'package:memo/utils/constants.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: frenchViolet,
+            borderRadius: BorderRadius.circular(100)
+          ),
+          child: Center(
+            child: Text("$appName.",
+            style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold,color: Colors.white),
+            ).animate().fadeIn().scale(delay: 1200.ms),
+          ),
+        ),
+      ),
+    );
   }
 }
